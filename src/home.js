ed0f2761157d066a9a0e493ef4d8b5fd9dc4f5b9
CebotariTigran <cebotaritigran@gmail.com>
2023-03-15 10:32:05 +0100

    Add menu and media query

--- src/home.js
+++ src/home.js
@@ -4,6 +4,8 @@ export function home() {
     const home = document.createElement('div');
     const headText = document.createElement('h2');
     const bodyText = document.createElement('p');
+    const imageDiv = document.createElement('div');
+
 
     home.classList.add('home')
 
@@ -12,12 +14,17 @@ export function home() {
     headText.textContent = 'About Alta';
     bodyText.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget neque vitae nisl tempus vehicula non sit amet lorem. Proin libero purus, varius sed posuere nec, egestas sed metus. Aenean sollicitudin nec tellus id tincidunt. Fusce ut feugiat felis. Morbi at bibendum nisi, non ornare libero. Pellentesque congue efficitur diam, ut faucibus eros convallis sed. Suspendisse mollis, risus quis laoreet tempor, nisl elit tristique lacus, vel laoreet mauris urna id erat. Etiam posuere, nunc eu condimentum posuere, lorem orci sollicitudin enim, fringilla sagittis tortor mi a odio. Vivamus at urna at diam dignissim rutrum non vitae velit. Nam mollis magna vel felis posuere finibus. Sed vulputate ligula ac facilisis dignissim.';
    
+    imageDiv.classList.add('home-image')
+
     homeContent.appendChild(headText);
     homeContent.appendChild(bodyText)
     homeContent.classList.add('home-content')
 
+
     main.replaceChildren();
-    home.appendChild(homeContent)
-    main.appendChild(home)
+
+    home.appendChild(homeContent);
+    main.appendChild(home);
+    main.appendChild(imageDiv);
     content.appendChild(main);
 }
--- src/menu.js
+++ src/menu.js
@@ -2,27 +2,51 @@ export function menu() {
     const content = document.querySelector('#content');
     const main = document.querySelector('.main');
     const menu = document.createElement('div');
-    menu.classList.add('menu')
+    menu.classList.add('menu');
     const menuItem = document.createElement('div');
-    menuItem.classList.add('item-one')
+    const menuText = document.createElement('div');
+    menuText.classList.add('text-one');
+    menuText.textContent= 'Pasta Plate'
+    menuItem.classList.add('item-one');
 
     const menuItemTwo = document.createElement('div');
-    menuItemTwo.classList.add('item-two')
+    const menuTextTwo = document.createElement('div');
+    menuTextTwo.classList.add('text-two');
+    menuTextTwo.textContent= 'Pasta Pesto'
+    menuItemTwo.classList.add('item-two');
 
     const menuItemThree = document.createElement('div');
-    menuItemThree.classList.add('item-three')
+    const menuTextThree = document.createElement('div');
+    menuTextThree.classList.add('text-three');
+    menuTextThree.textContent = 'Some More Pasta'
+    menuItemThree.classList.add('item-three');
 
     const menuItemFour = document.createElement('div');
-    menuItemFour.classList.add('item-four')
+    const menuTextFour = document.createElement('div');
+    menuTextFour.classList.add('text-four');
+    menuTextFour.textContent = 'Salad'
+    menuItemFour.classList.add('item-four');
 
     const menuItemFive = document.createElement('div');
-    menuItemFive.classList.add('item-five')
+    const menuTextFive = document.createElement('div');
+    menuTextFive.classList.add('text-five');
+    menuTextFive.textContent='Dolce'
+    menuItemFive.classList.add('item-five');
 
     const menuItemSix = document.createElement('div');
-    menuItemSix.classList.add('item-six')
+    const menuTextSix = document.createElement('div');
+    menuTextSix.textContent= 'A Soup?'
+    menuTextSix.classList.add('text-six');
+    menuItemSix.classList.add('item-six');
     
     main.replaceChildren();
-    menu.append(menuItem,menuItemTwo,menuItemThree,menuItemFour,menuItemFive,menuItemSix)
-    main.appendChild(menu)
+    menuItem.appendChild(menuText);
+    menuItemTwo.appendChild(menuTextTwo);
+    menuItemThree.appendChild(menuTextThree);
+    menuItemFour.appendChild(menuTextFour);
+    menuItemFive.appendChild(menuTextFive);
+    menuItemSix.appendChild(menuTextSix);
+    menu.append(menuItem,menuItemTwo,menuItemThree,menuItemFour,menuItemFive,menuItemSix);
+    main.appendChild(menu);
     content.appendChild(main);
 }
